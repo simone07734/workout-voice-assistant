42c7186bc9b059c2144c196be23e5cede1134434
simone07734 <simone.m.laney@gmail.com>
2025-02-19 02:10:11 -0800

    Test speech recognition

---
+++ speech_recognition_test.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+     
+with sr.Microphone() as source:     
+    print("Listening...")
+    r.pause_threshold = 1
+    audio = r.listen(source)
+  
+try:
+    print("Recognizing...")   
+    query = r.recognize_faseter_whisper(audio)
+    print(f"User said: {query}\n")
+  
+except Exception as e:
+    print(e)    
+    print("Unable to Recognize your voice.")  
