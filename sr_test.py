9e75e092404f5dc48e6f2e1e2244377865550b90
simone07734 <simone.m.laney@gmail.com>
2025-02-21 15:33:16 -0800

    change voice setting and recognizer model

--- sr_test.py
+++ sr_test.py
@@ -1,15 +1,15 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-     
-with sr.Microphone() as source:     
+
+with sr.Microphone(device_index = 1) as source:     
     print("Listening...")
-    r.pause_threshold = 1
+    r.pause_threshold = 2
     audio = r.listen(source)
   
 try:
     print("Recognizing...")   
-    query = r.recognize_faseter_whisper(audio)
+    query = r.recognize_vosk(audio)
     print(f"User said: {query}\n")
   
 except Exception as e:
