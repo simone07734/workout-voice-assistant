8675e25ac6b2e406cd064fd758bed1417b6c8d72
simone07734 <simone.m.laney@gmail.com>
2025-03-03 15:07:32 -0800

    Made narration run on Jetson Nano
    * modified narration to run on Jetson Nano
    * got rid of tts testing file
    * formatted output for better compatibility with narration
    * adjusted logging to work with new file location

--- workout_selection.py
+++ workout_selection.py
@@ -5,26 +5,40 @@ import queue # to repeatedly narrate
 from datetime import datetime # to get current date and time
 import json # to access workout files
 import pyttsx3 # text-to-speech library which allows program to speak
+from pydub import AudioSegment
+from pydub.playback import play
 
-engine = pyttsx3.init()
-# function to narrate text
+workout_folder_path = './workout_files'
+workout_logs_path = './workout_logs/workout_log.json'
+
+
+# Initialize tezt-to-speech engine. This uses espeak voice 23 to run on Linux. Different OS are compatible with a different engine, which will come with different voices to choose from.
+engine = pyttsx3.init('espeak')
+engine.setProperty('volume',1.0) 
+engine.setProperty('rate',190)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[23].id)
+
 
 def narrate (text):
-    engine.say(text)
+    engine.save_to_file(text, "narrate.wav")
     engine.runAndWait()
-
+    engine.save_to_file(" ", "overflow.wav") # for sound that doesn't get saved in the first file
+    engine.runAndWait()
+    speech = AudioSegment.from_wav("narrate.wav")
+    play(speech)
+    overflow = AudioSegment.from_wav("overflow.wav")
+    play(overflow)
 
 # retrieves all workout files fromm workout_files folder
-workout_folder_path = './workout_files'
 def get_workout_files (workout_folder_path):
     all_files = os.listdir(workout_folder_path)
     # removes .json from output
     workout_names = [os.path.splitext(f)[0] for f in all_files]
-   # print(workout_names)
     return workout_names
 
 # retrieves workout data from json file that was chosen by the user
-def retrieve_workouts(workout_file_path):
+def retrieve_workout(workout_file_path):
     try:
         with open(workout_file_path, 'r') as file:
             workout_data = json.load(file)
@@ -40,8 +54,9 @@ def select_workout(workout_folder_path):
     workout_files = get_workout_files(workout_folder_path)
     # narrate all options
     narrate ("Here are your workout options:")
+    # TODO: .sync should not be an option listed. make sure to only select valid workouts because reading from .sync would be bad
     for i, workout_file in enumerate (workout_files, start = 1):
-        narrate(f"{i}. {workout_file}")
+        narrate(f"number {i} is {workout_file}")
 
     # prompt user to select workout
     narrate ("Please choose a workout by saying the number.")
@@ -52,7 +67,7 @@ def select_workout(workout_folder_path):
     selected_workout_file = os.path.join(workout_folder_path, workout_files[user_choice])
     selected_workout_file = selected_workout_file + '.json'
     # retrieve and return data from the chosen workout
-    return retrieve_workouts(selected_workout_file)
+    return retrieve_workout(selected_workout_file)
 
 # function to wait for user response regarding when they want to start the exercise
 # uses threading to allow the prompt to repeat every 15 seconds while waiting for input (parallel execution)
@@ -88,6 +103,7 @@ def input_available():
     return select.select([sys.stdin], [], [], 0.1)[0]
 
 # logs workout details (name, difficulty, and date) to json
+
 def log_workout(workout_name, difficulty):
     log_entry = {
         "workout_name": workout_name,
@@ -107,7 +123,7 @@ def log_workout(workout_name, difficulty):
     
     # write the updated data to the file
     try:
-        with open("workout_log.json", "w") as file:
+        with open(workout_logs_path, "w") as file:
             json.dump(log_data, file, indent=4)
     except Exception as e:
         narrate(f"Error writing to file: {e}")  # Debugging: Catch and display any errors
@@ -120,11 +136,14 @@ def do_workout(workout_data):
     num_exercises = len(workout_data['exercises'])
     for i, exercise in enumerate(workout_data['exercises']):
         if i == 0:
-            narrate(f"First exercise is {exercise['name']}. We will do {exercise['sets']} sets of {exercise['reps']} reps.")
+            narrate(f"First exercise is {exercise['name']}") 
+            narrate(f"We will do {exercise['sets']} sets of {exercise['reps']} reps.")
         elif i == num_exercises - 1:
-            narrate(f"Last exercise is {exercise['name']}. We will do {exercise['sets']} sets of {exercise['reps']} reps.")
+            narrate(f"Last exercise is {exercise['name']}.")
+            narrate(f"We will do {exercise['sets']} sets of {exercise['reps']} reps.")
         else:
-            narrate(f"Next exercise is {exercise['name']}. We will do {exercise['sets']} sets of {exercise['reps']} reps.")
+            narrate(f"Next exercise is {exercise['name']}.")
+            narrate(f"We will do {exercise['sets']} sets of {exercise['reps']} reps.")
         ready = False
         while not ready:
             ready = wait_for_user_response()
@@ -138,18 +157,20 @@ def do_workout(workout_data):
                 time.sleep(2)
 
             if set_num < exercise['sets']:
-                narrate("Rest for 20 seconds.")
+                narrate("Rest for 5 seconds.")
                 time.sleep(5)
                 # CHANGE
 
         narrate(f"Finished {exercise['name']}")
 
-    narrate("Please rate the difficulty of this workout from 1 to 10, with 1 being the easiest and 10 being the hardest.")
+    narrate("Please rate the difficulty of this workout from 1 to 5")
+    narrate("with 1 being the easiest and 5 being the hardest")
     user_rating = int(input())
 
 
     log_workout(workout_data['workout_title'], user_rating)  # Log workout name and user rating as difficulty
-    narrate(f"Thank you for your feedback! Workout '{workout_data['workout_title']}' has been logged with a difficulty rating of {user_rating}.")
+    narrate(f"Thank you for your feedback!")
+    narrate(f"Workout {workout_data['workout_title']} has been logged with a difficulty rating of {user_rating}.")
 
     
 # test
