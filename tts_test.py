3cdb6d70ad1c7450ba23a15ab249b94aea3410ac
simone07734 <simone.m.laney@gmail.com>
2025-03-03 14:14:05 -0800

    working playback of tts

--- tts_test.py
+++ tts_test.py
@@ -1,12 +1,18 @@
 import pyttsx3
+from playsound import playsound
 
-engine = pyttsx3.init()
+engine = pyttsx3.init('espeak')
+
 engine.setProperty('volume',1.0) 
-engine.setProperty('rate',200)
+engine.setProperty('rate',180)
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-engine.say("Hello! I am FitEdge. What workout would you like to do today?")
+
+engine.setProperty('voice', voices[23].id)
+engine.save_to_file("Hello I am FitEdge What workout would you like to do today?", "test.wav")
 engine.runAndWait()
+
+playsound("test.wav")
+
 
 rate = engine.getProperty('rate')  
 print (rate) 
